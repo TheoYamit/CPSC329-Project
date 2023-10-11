c76575d9429a99c7a43b3d319b489a5b31a8de39
Arnav Bhalla <71564120+arnavbhalla-byte@users.noreply.github.com>
2023-10-11 17:44:16 -0600

    Create tester.js

---
+++ tester.js
@@ -0,0 +1,55 @@
+<!--##Author - Arnav Bhalla-->
+<!--30173316-->
+document.addEventListener('DOMContentLoaded', function() {
+    const checkButton = document.getElementById('checkButton');
+    checkButton.addEventListener('click', checkPasswordStrength);
+
+    function checkPasswordStrength() {
+        const password = document.getElementById('passwordInput').value;
+        const strengthIndicator = document.getElementById('passwordStrength');
+        const timeToCrackIndicator = document.getElementById('timeToCrack');
+
+        // Reset the output display
+        strengthIndicator.textContent = '';
+        timeToCrackIndicator.textContent = '';
+
+        // Define a scoring system
+        fetch('commonPasswords.txt')
+            .then(response => response.text())
+            .then(data => {
+                const commonPasswords = data.split('\n');
+
+                // Interpret the strength score
+
+                // Estimate time to crack with a brute-force attack
+                const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+                const passwordLength = password.length;
+                const totalPossibilities = characters.length ** passwordLength;
+                const averageGuessesPerSecond = 1000000000; // Assumption: 1 billion guesses per second
+                const secondsToCrack = totalPossibilities / (2 * averageGuessesPerSecond); // Divide by 2 for average cracking time
+
+                // Convert seconds to a more human-readable format
+                let timeToCrackText = '';
+                if (secondsToCrack < 60) {
+                    timeToCrackText = `${secondsToCrack} seconds`;
+                } else if (secondsToCrack < 3600) {
+                    timeToCrackText = `${Math.round(secondsToCrack / 60)} minutes`;
+                } else if (secondsToCrack < 86400) {
+                    timeToCrackText = `${Math.round(secondsToCrack / 3600)} hours`;
+                } else if (secondsToCrack < 604800) {
+                    timeToCrackText = `${Math.round(secondsToCrack / 86400)} days`;
+                } else {
+                    timeToCrackText = `${Math.round(secondsToCrack / 604800)} weeks`;
+                }
+
+                timeToCrackIndicator.textContent = `Estimated Time to Crack: ${timeToCrackText}`;
+
+                // Check against the list of common passwords
+                if (commonPasswords.includes(password)) {
+                    strengthIndicator.textContent = "Password Strength: Very Weak (Common Password)";
+                    timeToCrackIndicator.textContent = "Estimated Time to Crack: Instant";
+                    return;
+                }
+            });
+    }
+});
